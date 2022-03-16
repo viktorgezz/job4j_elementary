2528ede0d74c42435fdda1a615312d18b953b7b9
AndreyVerov <viktorgezz@mail.ru>
2022-03-16 11:35:35 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,15 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        System.out.println("1* 9 = 9");
+        System.out.println("2 * 9 = 18");
+        System.out.println("3 * 9 = 27");
+        System.out.println("4 * 9 = 36");
+        System.out.println("5 * 9 = 45");
+        System.out.println("6 * 9 = 54");
+        System.out.println("7 * 9 = 63");
+        System.out.println("8 * 9 = 72");
+        System.out.println("9 * 9 = 81");
+    }
+}
