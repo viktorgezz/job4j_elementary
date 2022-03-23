a575d25ea16924dfbdf3589e70701d84326facf9
AndreyVerov <viktorgezz@mail.ru>
2022-03-23 15:39:40 +0300

    1. Определите тип данных для переменных. [#172634]

--- src/main/java/ru/job4j/Variable.java
+++ src/main/java/ru/job4j/Variable.java
@@ -2,8 +2,13 @@ package ru.job4j;
 
 public class Variable {
     public static void main(String[] args) {
-        int age = 18;
-        float length = 78.0332F;
-        boolean baby = true;
+        int age;
+        String floor;
+        int growth;
+        int brick;
+        long bacteria;
+        long distanceSpaceObjects;
+        boolean workPermit;
+        byte fileSize;
     }
 }
