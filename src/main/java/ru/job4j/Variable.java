1064c6a1dc908ca7111440e229314de114ddad00
AndreyVerov <viktorgezz@mail.ru>
2022-03-23 15:51:12 +0300

    2. Переменные. [#172635]

--- src/main/java/ru/job4j/Variable.java
+++ src/main/java/ru/job4j/Variable.java
@@ -2,13 +2,13 @@ package ru.job4j;
 
 public class Variable {
     public static void main(String[] args) {
-        int age;
-        String floor;
-        int growth;
-        int brick;
-        long bacteria;
-        long distanceSpaceObjects;
-        boolean workPermit;
-        byte fileSize;
+        int age = 18;
+        String floor = "M";
+        int growth = 170;
+        int brick = 500;
+        long bacteria = 3_000_000_000L;
+        long distanceSpaceObjects = 21_000_000_000L;
+        boolean workPermit = true;
+        byte fileSize = 126;
     }
 }
