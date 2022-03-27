63b5871fb43b020efd0aa110311393592cc0be1e
AndreyVerov <viktorgezz@mail.ru>
2022-03-27 17:39:11 +0300

    -

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -1,14 +1,14 @@
 package ru.job4j;
 
 public class Calculator {
-    public static void plus(int first, int second){
+    public static void plus(int first, int second) {
         int result = first + second;
         System.out.println(result);
     }
 
     public static void main(String[] args) {
-        Calculator.plus(100,500);
-        Calculator.plus(4,2);
-        Calculator.plus(3,6);
+        Calculator.plus(100, 500);
+        Calculator.plus(4, 2);
+        Calculator.plus(3, 6);
     }
 }
