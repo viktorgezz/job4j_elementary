f2bad9a040bda674a35ef0b6141403002a0136cf
AndreyVerov <viktorgezz@mail.ru>
2022-05-03 12:09:45 +0300

    5.7. Ипотека [#156315 #296273]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,14 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
-        boolean debt = true;
-
-        while (debt) {
+        while (amount > 0) {
             amount =  amount * (1 + percent / 100) - salary;
             year++;
-            if (amount <= 0) {
-                debt = false;
-            }
         }
         return year;
     }
