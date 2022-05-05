3020815366225240a7ebd6a6208e482c542f7e24
AndreyVerov <viktorgezz@mail.ru>
2022-05-05 21:55:36 +0300

    6.0.1. Объявление массива. [#3613 #296608]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,16 +5,5 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-
-        System.out.println("Размер массива ages равен: " + ages.length);
-        System.out.println("Размер массива surname равен: " + surnames.length);
-        System.out.println("Размер массива prices: " + prices.length);
-
-        String[] names = new String[4];
-        names[0] = "Viktor";
-        names[1] = "Nastya";
-        names[2] = "Artem";
-        names[3] = "Rita";
-        System.out.printf("%s, %s, %s, %s.", names[0], names[1], names[2], names[3]);
     }
 }
